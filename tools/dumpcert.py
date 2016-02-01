f18fb66374636f5be3ba3bc9601a38e4a5ed9d9c
Paul Baecher <pbaecher@gmail.com>
2016-02-01 21:54:00 +0100

    revise dumpcert and rename tools to scripts

--- tools/dumpcert.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from struct import unpack
-from sys import stdin, stdout
-
-data = stdin.read()
-stdout.write('{')
-for c in data:
-    stdout.write('{}, '.format(unpack('b', c)[0]))
-stdout.write('}\n')
