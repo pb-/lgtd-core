4c074e5fb06a26ca4b0d6044fb152694df99fae1
Paul Baecher <pbaecher@gmail.com>
2016-02-23 18:10:44 +0100

    d: proper logging, make --daemon more useful

--- lgtd/d.py
+++ lgtd/d.py
@@ -1,9 +1,10 @@
+import logging
+import os
 from argparse import ArgumentParser
 from collections import OrderedDict, defaultdict
 from datetime import date, datetime, timedelta
 from getpass import getpass
 from json import dumps, loads
-from os import fork
 
 import pyinotify
 from tornado import ioloop, web
@@ -14,6 +15,8 @@ from .lib.crypto import CommandCipher, hash_password
 from .lib.db import ClientDatabase
 from .lib.util import (ensure_data_dir, ensure_lock_file, get_data_dir,
                        get_local_config, get_lock_file)
+
+logger = logging.getLogger(__name__)
 
 
 class StateManager(object):
@@ -50,7 +53,7 @@ class StateManager(object):
             for line, app_id, offset in self.db.read_all(self.offsets):
                 cmd = Command.parse(self.cipher.decrypt(line, app_id, offset))
                 cmd.apply(self.state)
-                print('executing: {}'.format(str(cmd)))
+                logger.debug('executing: {}'.format(str(cmd)))
 
             self.offsets = offsets
             return True
@@ -105,19 +108,19 @@ class GTDSocketHandler(WebSocketHandler):
 
     def open(self):
         self.clients.append(self)
-        print("WebSocket opened")
+        logger.debug('client connected')
 
     def on_message(self, message):
         data = loads(message)
-        print('got msg {}'.format(data))
+        logger.debug('message {}'.format(data))
 
         if data['msg'] == 'request_state':
-            print('replying with state')
+            logger.debug('replying with state')
             state = self.state_manager.render_state(
                 data['tag'].encode('utf-8'))
             self.write_message(dumps({'msg': 'state', 'state': state}))
         elif data['msg'] == 'push_commands':
-            print('pushing some commands')
+            logger.debug('pushing some commands')
             self.state_manager.push_commands(data['cmds'])
 
     def notify(self):
@@ -125,12 +128,13 @@ class GTDSocketHandler(WebSocketHandler):
 
     def on_close(self):
         self.clients.remove(self)
-        print("WebSocket closed")
+        logger.debug('client disconnected')
 
 
 def change_callback(notifier):
-    print('change?')
+    logger.debug('change?')
     if notifier.state_manager.notify():
+        logger.debug('change - notifying clients')
         for client in notifier.clients:
             client.notify()
 
@@ -164,20 +168,12 @@ def parse_args():
     parser = ArgumentParser(description='local data service for lgtd')
     parser.add_argument(
         '-d', '--daemon', action='store_true', help='daemonize after asking '
-        'for the encryption passphrase')
+        'for the encryption passphrase and starting to listen')
     return parser.parse_args()
 
 
-def run():
+def run_daemon(args, key, pipe_write):
     clients = []
-    args = parse_args()
-    key = hash_password(getpass())
-
-    if args.daemon:
-        pid = fork()
-        if pid:
-            return 0
-
     config = get_local_config()
     state_manager = StateManager(
         config['app_id'],
@@ -200,6 +196,35 @@ def run():
             'state_manager': state_manager}),
     ])
     app.listen(9001, address='127.0.0.1')
+    logger.info('listening')
+
+    if pipe_write:
+        # notify parent proc that we are listening now
+        os.close(pipe_write)
 
     schedule_midnight(ioloop.IOLoop.current(), clients)
     ioloop.IOLoop.current().start()
+
+
+def run():
+    args = parse_args()
+    key = hash_password(getpass())
+
+    if args.daemon:
+        logger.setLevel(logging.INFO)
+        handler = logging.handlers.SysLogHandler('/dev/log')
+        handler.setFormatter(logging.Formatter('%(name)s %(message)s'))
+        logger.addHandler(handler)
+
+        pipe_read, pipe_write = os.pipe()
+        pid = os.fork()
+        if pid:
+            os.close(pipe_write)
+            os.read(pipe_read, 1)  # wait until we are listening
+            return 0
+        else:
+            os.close(pipe_read)
+    else:
+        logging.basicConfig(level=logging.DEBUG)
+
+    run_daemon(args, key, pipe_write if args.daemon else None)
