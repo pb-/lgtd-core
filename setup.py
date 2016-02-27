24ac34634432fa005cb37d0bfafd799ab16f3d28
Paul Baecher <pbaecher@gmail.com>
2016-02-27 16:37:26 +0100

    make client dependencies extras

--- setup.py
+++ setup.py
@@ -10,23 +10,27 @@ setup(
     url='https://github.com/pb-/lgtd-suite',
     packages=find_packages('.'),
     install_requires=[
-        'cryptography >=1.2.1,<2',
-        'pyinotify >=0.9.6,<1',
-        'requests >=2.9.1,<3',
         'tornado >=4.3',
-        'websocket-client >=0.35.0,<1',
     ],
+    extras_require={
+        'client': [
+            'cryptography >=1.2.1,<2',
+            'pyinotify >=0.9.6,<1',
+            'requests >=2.9.1,<3',
+            'websocket-client >=0.35.0,<1',
+        ],
+    },
     scripts=[
         'scripts/lgtd',
     ],
     entry_points={
         'console_scripts': [
-            'lgtd_ui = lgtd.ui:run',
-            'lgtd_i3 = lgtd.i3:run',
-            'lgtd_d = lgtd.d:run',
-            'lgtd_sync = lgtd.sync.client:run',
+            'lgtd_ui = lgtd.ui:run [client]',
+            'lgtd_i3 = lgtd.i3:run [client]',
+            'lgtd_d = lgtd.d:run [client]',
+            'lgtd_sync = lgtd.sync.client:run [client]',
             'lgtd_syncd = lgtd.sync.server:run',
-            'lgtd_dbadm = lgtd.tools.dbadm:run',
+            'lgtd_dbadm = lgtd.tools.dbadm:run [client]',
         ],
     },
 )
