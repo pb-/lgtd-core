8871cb5f4cfa05e63e6cdd6a19e196d2b73a626b
Paul Baecher <pbaecher@gmail.com>
2016-04-10 01:28:13 +0200

    i3: make it work again

--- lgtd/i3.py
+++ lgtd/i3.py
@@ -1,11 +1,26 @@
+import hmac
 import sys
 from json import loads
 
 from websocket import create_connection
 
+from .lib.util import get_local_config
+
+
+def authenticate(ws, config):
+    data = loads(ws.recv())
+    mac = hmac.new(
+        str(config['local_auth']), str(data['nonce'])).digest().encode('hex')
+    ws.send('{"msg": "auth_response", "mac": "%s"}' % mac)
+    data = loads(ws.recv())
+    if data['msg'] != 'authenticated':
+        raise Exception('authentication failed')
+
 
 def run():
+    config = get_local_config()
     ws = create_connection('ws://127.0.0.1:9001/gtd')
+    authenticate(ws, config)
     ws.send('{"msg": "request_state", "tag": "inbox"}')
     data = loads(ws.recv())
     ws.close()
@@ -16,3 +31,4 @@ def run():
             sys.stdout.write(' ')
         count = str(tags[tag]) if tag in tags else '?'
         sys.stdout.write('{}:{}'.format(tag, count))
+    sys.stdout.write('\n')
